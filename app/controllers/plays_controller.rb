91df1b6bc61680ed141d03615fdba6f85d2727a1
emesc <tahder77@gmail.com>
2016-10-04 11:27:04 +1100

    Added CRUD actions with new and create view files.

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,30 +1,51 @@
 class PlaysController < ApplicationController
 
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
+
   def index
+    @plays = Play.order("created_at DESC")
   end
 
   def new
-    
+    @play = Play.new    
   end
 
   def create
-
+    @play = Play.new(play_params)
+    if @play.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
-    
   end
 
   def edit
-    
   end
 
   def update
-    
+    if @play.update_attributes(play_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-    
+    @play.destroy
+    redirect_to root_path
   end
+
+  private
+
+    def find_play
+      @play = Play.find(params[:id])
+    end
+
+    def play_params
+      params.require(:play).permit(:title, :description, :director)
+    end
 
 end
